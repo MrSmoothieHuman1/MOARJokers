f65a7e78541cce8ae46ed54f8af4164dacce94c2
MrSmoothieHuman1 <109899733+MrSmoothieHuman1@users.noreply.github.com>
2024-10-31 13:39:35 +0000

    Fixed Prime Number Paul not actually triggering aces

--- MoarJokers.lua
+++ MoarJokers.lua
@@ -682,7 +682,7 @@ SMODS.Atlas {
     end,
     calculate = function(self, card, context)
       if context.individual
-      and (context.other_card:get_id() == 2 or context.other_card:get_id() == 3 or context.other_card:get_id() == 5 or context.other_card:get_id() == 7 or context.other_card:get_id() == 11) then
+      and (context.other_card:get_id() == 2 or context.other_card:get_id() == 3 or context.other_card:get_id() == 5 or context.other_card:get_id() == 7 or context.other_card:get_id() == 14) then
         return {
           mult = card.ability.extra.mult,
           chips = card.ability.extra.chips,
